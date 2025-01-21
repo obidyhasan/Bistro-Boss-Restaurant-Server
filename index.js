3467d60daf2fa09f9222b30aba1febda8c42b1c5
obidyhasan <obidyhasan@gmail.com>
2025-01-21 18:55:42 +0600

    include vercel on server side

--- index.js
+++ index.js
@@ -310,8 +310,8 @@ async function run() {
       res.send(result);
     });
 
-    await client.connect();
-    await client.db("admin").command({ ping: 1 });
+    // await client.connect();
+    // await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
